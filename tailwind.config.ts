8d9f1204e5d6872dd2f53061f0df01ddb1a9086e
evelo <evelo0702@gmail.com>
2025-02-22 22:21:03 +0900

    feat: 기본 설정 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,22 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      height: {
+        "95vh": "95vh",
+        "90vh": "90vh",
+        "85vh": "85vh",
+        "80vh": "80vh",
+        "70vh": "70vh",
       },
     },
   },
