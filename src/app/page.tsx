19d35f46e555f807f9f494b9fd6f3d736fe5cf5b
evelo <evelo0702@gmail.com>
2025-02-22 22:20:30 +0900

    feat: 보드와 카드 데이터 병합

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <div></div>;
+import BoardList from "../components/BoardList";
+import { fetchAllData } from "../lib/fetchAllData";
+import { Board, BoardWithTodos, Todo } from "../types";
+
+export default async function Home() {
+  const { boards, todos, users, labels } = await fetchAllData();
+
+  // 정규화된 구조의 보드와 카드 데이터를 합치는 작업
+  const boardsWithTodos: BoardWithTodos[] = boards.map((board: Board) => ({
+    ...board,
+    todos: todos
+      .filter((todo: Todo) => todo.boardId === board._id)
+      .sort((a: Todo, b: Todo) => a.order - b.order),
+  }));
+  return (
+    <div className="">
+      <BoardList boardsWithTodos={boardsWithTodos} />
+    </div>
+  );
 }
