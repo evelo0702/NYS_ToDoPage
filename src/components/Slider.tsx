923988a1f79ef793b3ce6d52de59735cae6c9efe
evelo <evelo0702@gmail.com>
2025-02-23 23:48:31 +0900

    feat: 보드 슬라이더 컴포넌트 생성

---
+++ src/components/Slider.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { RefObject } from "react";
+interface SliderProps {
+  children: React.ReactNode;
+  scrollContainerRef: RefObject<HTMLDivElement | null>;
+}
+export default function Slider({ children, scrollContainerRef }: SliderProps) {
+  const scroll = (direction: "left" | "right") => {
+    if (scrollContainerRef.current) {
+      const boardWidth = scrollContainerRef.current.offsetWidth / 3 + 1;
+      scrollContainerRef.current.scrollBy({
+        left: direction === "left" ? -boardWidth : boardWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+  return (
+    <div>
+      <button
+        className="absolute left-2 z-10 p-3 "
+        onClick={() => scroll("left")}
+      >
+        ◀
+      </button>
+
+      {children}
+
+      <button
+        className="absolute right-2 z-10 p-3"
+        onClick={() => scroll("right")}
+      >
+        ▶
+      </button>
+    </div>
+  );
+}
