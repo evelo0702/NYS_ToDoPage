bcdecfbceb23cfdf0d532d62a1b9fd4e6ae32f3b
evelo <evelo0702@gmail.com>
2025-02-24 01:01:31 +0900

    del: 불필요한 상태 삭제

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,35 +1,33 @@
 "use client";
 
-import { useState } from "react";
-
 interface SearchBarProps {
-  setSearchQuery: (query: string | null) => void;
+  searchQuery: string | "";
+  setSearchQuery: (query: string | "") => void;
 }
 
-export default function SearchBar({ setSearchQuery }: SearchBarProps) {
-  const [searchText, setSearchText] = useState<string>("");
-
+export default function SearchBar({
+  searchQuery,
+  setSearchQuery,
+}: SearchBarProps) {
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const query = e.target.value;
-    setSearchText(query);
-    setSearchQuery(query ? query : null);
+    setSearchQuery(query ? query : "");
   };
 
   const handleSearchClear = () => {
-    setSearchText("");
-    setSearchQuery(null);
+    setSearchQuery("");
   };
 
   return (
-    <div className="flex items-center border p-2 rounded-lg shadow-md bg-white w-1/2">
+    <div className="flex items-center border p-2 rounded-lg shadow-md bg-white w-1/3 mx-4">
       <input
         type="text"
-        value={searchText}
+        value={searchQuery}
         onChange={handleSearchChange}
         placeholder="검색어를 입력하세요..."
         className="p-2 w-full border-none outline-none"
       />
-      {searchText && (
+      {searchQuery && (
         <button
           onClick={handleSearchClear}
           className="ml-2 text-2xl text-red-700 font-bold"
--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -5,76 +5,78 @@ import { Label } from "../types";
 import { useBoardStore } from "../store/globalStore";
 
 interface SearchListProps {
+  searchLabel: Label | null;
+  searchDate: string | null;
   setSearchLabel: (label: Label | null) => void;
   setSearchDate: (date: string | null) => void;
 }
 
 export default function SearchList({
+  searchLabel,
+  searchDate,
   setSearchLabel,
   setSearchDate,
 }: SearchListProps) {
   const { labelList } = useBoardStore();
-  const [selectedLabel, setSelectedLabel] = useState<Label | null>(null);
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedDate, setSelectedDate] = useState<string | null>(null);
 
   const toggleDropdown = () => setIsOpen(!isOpen);
 
   const handleLabelSelect = (label: Label | null) => {
-    if (selectedLabel?._id === label?._id) return;
-    setSelectedLabel(label);
+    if (searchLabel?._id === label?._id) return;
     setSearchLabel(label);
     setIsOpen(false);
   };
 
   const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const date = e.target.value;
-    setSelectedDate(date);
     setSearchDate(date); // 선택된 날짜로 필터링
   };
 
   return (
-    <div className="relative w-1/4 flex items-center space-x-4">
-      {/* 라벨 선택 */}
-      <div className="flex-1">
+    <div className="relative w-1/3 flex items-center space-x-4 mx-4">
+      <div className="flex-1 ">
         <div
           className="flex items-center justify-between cursor-pointer border p-2 rounded-lg shadow-md bg-white"
           onClick={toggleDropdown}
         >
-          <span>
-            {selectedLabel ? (
+          <div>
+            {searchLabel ? (
               <div className="flex items-center justify-center">
                 <div
                   className={`w-4 h-4 mr-2 rounded-sm ${
-                    selectedLabel._id === "67b9a48f67b4270871740d0b"
+                    searchLabel._id === "67b9a48f67b4270871740d0b"
                       ? "bg-red-200"
-                      : selectedLabel._id === "67b9a48f67b4270871740d0c"
+                      : searchLabel._id === "67b9a48f67b4270871740d0c"
                       ? "bg-sky-200"
-                      : selectedLabel._id === "67b9a48f67b4270871740d0d"
+                      : searchLabel._id === "67b9a48f67b4270871740d0d"
                       ? "bg-green-200"
-                      : selectedLabel._id === "67b9a48f67b4270871740d0e"
+                      : searchLabel._id === "67b9a48f67b4270871740d0e"
                       ? "bg-yellow-200"
-                      : selectedLabel._id === "67b9a48f67b4270871740d0f"
+                      : searchLabel._id === "67b9a48f67b4270871740d0f"
                       ? "bg-indigo-200"
-                      : ""
+                      : "bg-black"
                   }`}
                 ></div>
-                {selectedLabel.title}
+                {searchLabel.title}
               </div>
             ) : (
-              "전체 보기"
+              <div className="flex items-center justify-center">
+                <div className={`w-4 h-4 mr-2 rounded-sm bg-black`}></div>
+                <span>전체 업무</span>
+              </div>
             )}
-          </span>
+          </div>
           <span className="text-lg">▼</span>
         </div>
 
         {isOpen && (
-          <div className="absolute w-full mt-2 bg-white shadow-lg rounded-lg max-h-60 overflow-y-auto z-10">
+          <div className="absolute w-1/2 mt-2 bg-white shadow-lg rounded-lg max-h-60 overflow-y-auto z-10">
             <div
               className="flex items-center p-3 cursor-pointer hover:bg-gray-100 rounded-md"
               onClick={() => handleLabelSelect(null)}
             >
-              <div className="w-4 h-4 mr-2 rounded-sm bg-black"></div>전체 보기
+              <div className="w-4 h-4 mr-2 rounded-sm bg-black"></div>전체 업무
             </div>
             {labelList.map((label: Label) => (
               <div
@@ -108,7 +110,7 @@ export default function SearchList({
       <div className="flex-1">
         <input
           type="date"
-          value={selectedDate || ""}
+          value={searchDate || ""}
           onChange={handleDateChange}
           className="p-2 border rounded w-full"
         />
