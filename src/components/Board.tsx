50b86cf1ef32a96e0018ca9e188306911d409f58
evelo <evelo0702@gmail.com>
2025-02-23 17:51:05 +0900

    feat: board, todo 컴포넌트 기본 디자인

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,10 +1,30 @@
-'use client';
+"use client";
 
-export default function Board() {
+import type { BoardWithTodos } from "../types";
+import Todo from "./Todo";
+
+interface BoardProps {
+  data: BoardWithTodos;
+}
+
+export default function Board({ data }: BoardProps) {
   return (
-    <div>
-      <h1>Board</h1>
-      <p>This is a client component.</p>
+    <div className="bg-white rounded-xl shadow-lg p-4 h-full flex flex-col overflow-y-auto">
+      <div className="h-1/6">
+        <h1 className="text-lg font-semibold text-center">{data.title}</h1>
+        <button className="mt-2 w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition">
+          + 할 일 추가
+        </button>
+      </div>
+      <div className="mt-4 flex-1  space-y-2 p-2">
+        {data.todos.length > 0 ? (
+          data.todos.map((todo) => <Todo key={todo._id} data={todo} />)
+        ) : (
+          <div className="text-gray-400 text-center py-6">
+            할 일이 없습니다.
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/Todo.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+import { Todo as TodoType } from "../types";
+
+interface TodoProps {
+  data: TodoType;
+}
+
+export default function Todo({ data }: TodoProps) {
+  const [labelId] = useState("67b9a48f67b4270871740d0c");
+  return (
+    <div className="bg-gray-100 p-3 rounded-lg shadow-sm">
+      <div className="flex justify-between">
+        <h2 className="text-lg font-semibold mt-1">{data.title}</h2>
+        <div>
+          <button>수정</button>
+          <button className="ms-2">삭제</button>
+        </div>
+      </div>
+      <div className="text-xs text-gray-500 flex justify-between">
+        <span>{data.author}</span>
+        <span>{new Date(data.date).toLocaleDateString()}</span>
+      </div>
+      <div className="mt-2 flex flex-wrap gap-1">
+        <span
+          className={`text-xs px-2 py-1 ${
+            (labelId === "67b9a48f67b4270871740d0b" && "bg-red-200") ||
+            (labelId === "67b9a48f67b4270871740d0c" && "bg-sky-200") ||
+            (labelId === "67b9a48f67b4270871740d0d" && "bg-green-200") ||
+            (labelId === "67b9a48f67b4270871740d0e" && "bg-yellow-200") ||
+            (labelId === "67b9a48f67b4270871740d0f" && "bg-indigo-200")
+          } rounded-md`}
+        >
+          작업 전 업무
+        </span>
+      </div>
+      <p className="text-lg text-gray-600 mt-1 line-clamp-2">{data.content}</p>
+    </div>
+  );
+}
