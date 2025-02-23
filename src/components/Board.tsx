8c4f1bae01af8db3bffa4469a1d56ea24cfc4622
evelo <evelo0702@gmail.com>
2025-02-24 01:39:48 +0900

    update: css 변경

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -9,14 +9,14 @@ interface BoardProps {
 
 export default function Board({ data }: BoardProps) {
   return (
-    <div className="bg-white rounded-xl shadow-lg p-4 h-full flex flex-col overflow-y-auto">
+    <div className="bg-white rounded-xl shadow-lg p-4 h-full flex flex-col">
       <div className="h-1/6">
         <h1 className="text-lg font-semibold text-center">{data.title}</h1>
         <button className="mt-2 w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition">
           + 할 일 추가
         </button>
       </div>
-      <div className="mt-4 flex-1  space-y-2 p-2">
+      <div className="mt-4 flex-col space-y-2 p-2 h-full overflow-y-auto">
         {data.todos.length > 0 ? (
           data.todos.map((todo) => <Todo key={todo._id} data={todo} />)
         ) : (
