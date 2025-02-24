47136fb3d6d31027e3a89e6516216b585f70993c
evelo <evelo0702@gmail.com>
2025-02-25 07:35:29 +0900

    update: 자료구조 변경에따른 서버액션 호출 메서드 변경

--- src/lib/fetchAllData.ts
+++ src/lib/fetchAllData.ts
@@ -1,16 +1,14 @@
 import { getBoardsAction } from "../actions/boards/getBoards.action";
 import { getLabelsAction } from "../actions/labels/getLabels.action";
-import { getTodosAction } from "../actions/todos/getTodos.action";
 
 export async function fetchAllData() {
   try {
-    const [boards, todos, labels] = await Promise.all([
+    const [boards, labels] = await Promise.all([
       getBoardsAction(),
-      getTodosAction(),
       getLabelsAction(),
     ]);
 
-    return { boards, todos, labels };
+    return { boards, labels };
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error;
