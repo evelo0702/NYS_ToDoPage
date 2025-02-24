dacebd99038f456598a337e2d79be6e465fbade3
evelo <evelo0702@gmail.com>
2025-02-25 02:35:22 +0900

    feat: undo,endo 버튼 + 보드  추가,삭제,수정 기능 생성

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { BoardWithTodos, Label } from "../types";
 import Board from "./Board";
 import SearchBar from "./SearchBar";
 import SearchList from "./SearchList";
 import Slider from "./Slider";
-import { addBoard } from "../actions/boards/addBoard.action";
+import { useBoardStore } from "../store/globalStore";
+import { v4 as uuidv4 } from "uuid";
 
 interface BoardListProps {
   boardsWithTodos: BoardWithTodos[];
@@ -20,8 +21,19 @@ export default function BoardList({ boardsWithTodos, labels }: BoardListProps) {
   const [searchQuery, setSearchQuery] = useState<string | "">("");
   const [newBoardName, setNewBoardName] = useState<string | "">("");
 
+  const { boards, setBoards, setLabels, addBoard, undo, redo, past, future } =
+    useBoardStore();
+  useEffect(() => {
+    if (boardsWithTodos && labels) {
+      setBoards(boardsWithTodos);
+      setLabels(labels);
+    }
+  }, [boardsWithTodos, labels, setBoards, setLabels]);
+  useEffect(() => {
+    console.log("Boards changed:", boards);
+  }, [boards]);
   const filteredBoards = useMemo(() => {
-    return boardsWithTodos
+    return boards
       .map((board) => {
         // board.title에 쿼리가 포함되었는지 확인
         const matchesBoardTitle = searchQuery
@@ -61,46 +73,29 @@ export default function BoardList({ boardsWithTodos, labels }: BoardListProps) {
 
             return matchesLabel && matchesQuery && matchesDate;
           });
-
           // 필터링된 todos가 존재하면 해당 board 반환
           return { ...board, todos: filteredTodos };
         }
       })
       .filter((board) => board !== null)
       .sort((a, b) => a.order - b.order);
-  }, [boardsWithTodos, searchLabel, searchQuery, searchDate]);
+  }, [boards, searchLabel, searchQuery, searchDate]);
 
   const handleAddBoard = async () => {
     if (!newBoardName.trim()) return alert("보드 이름을 입력하세요.");
-
-    const response = await addBoard(newBoardName);
-
-    if (response.success) {
-      setNewBoardName("");
-    } else {
-      alert("보드 추가 실패: " + response.status);
-    }
+    const tempId = uuidv4();
+    const newBoard = {
+      _id: tempId,
+      title: newBoardName,
+      order: boards.length,
+      todos: [],
+    };
+    addBoard(newBoard);
+    setNewBoardName("");
   };
   return (
     <div className="w-full h-95vh grid grid-rows-12 mt-4 bg-gray-100">
       <header className="row-span-1 bg-white shadow-md p-4 flex items-center border-b justify-end">
-        <div className="h-1/2 flex items-center w-1/4">
-          <div className="flex items-center border p-2 rounded-lg shadow-md bg-white mx-4">
-            <input
-              type="text"
-              placeholder="보드 이름"
-              className="p-2 w-full border-none outline-none"
-              value={newBoardName}
-              onChange={(e) => setNewBoardName(e.target.value)}
-            />
-          </div>
-          <button
-            className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
-            onClick={handleAddBoard}
-          >
-            + 보드 추가
-          </button>
-        </div>
         <div className="w-3/4 flex justify-end">
           <button
             className="me-2 hover:bg-blue-200 p-2 rounded-md"
@@ -125,9 +120,50 @@ export default function BoardList({ boardsWithTodos, labels }: BoardListProps) {
           />
         </div>
       </header>
+      <div className="row-span-1 flex justify-between items-center px-8 ">
+        <div>
+          <button
+            disabled={past.length === 0}
+            className={`${
+              past.length === 0
+                ? "bg-black text-white cursor-not-allowed opacity-50"
+                : "bg-white text-black "
+            } border border-black py-2 px-6 rounded-lg transition duration-300 me-2`}
+            onClick={undo}
+          >
+            실행 취소
+          </button>
+          <button
+            disabled={future.length === 0}
+            className={`${
+              future.length === 0
+                ? "bg-black text-white cursor-not-allowed opacity-50"
+                : "bg-white text-black "
+            } border border-black py-2 px-6 rounded-lg transition duration-300`}
+            onClick={redo}
+          >
+            재실행
+          </button>
+        </div>
+        <div className="flex border shadow-md w-1/4">
+          <input
+            type="text"
+            placeholder="보드 이름"
+            className="p-2 w-full border-none outline-none rounded-lg"
+            value={newBoardName}
+            onChange={(e) => setNewBoardName(e.target.value)}
+          />
+          <button
+            className="p-2 w-1/2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
+            onClick={handleAddBoard}
+          >
+            + 보드 추가
+          </button>
+        </div>
+      </div>
 
       {/* 좌우 버튼 & 보드 리스트 */}
-      <div className="relative flex row-span-11 items-center ">
+      <div className="relative flex row-span-10 items-center">
         {filteredBoards.length > 0 ? (
           <Slider scrollContainerRef={scrollContainerRef}>
             <div
@@ -135,7 +171,10 @@ export default function BoardList({ boardsWithTodos, labels }: BoardListProps) {
               className="flex w-full h-full overflow-x-auto py-6 scroll-smooth"
             >
               {filteredBoards.map((board) => (
-                <div className="min-w-[30%] mx-[1.5%]" key={board._id}>
+                <div
+                  className="min-w-[30%] mx-[1.5%]"
+                  key={`${board._id}-${board.title}`}
+                >
                   <Board data={board} />
                 </div>
               ))}
