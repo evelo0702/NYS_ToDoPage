da025f5fb3a11ee2753773fdfcef3e8ba99fc55e
evelo <evelo0702@gmail.com>
2025-02-25 07:34:10 +0900

    update: 자료구조 변경에 따른 상태관리 메서드 변경

--- src/store/globalStore.ts
+++ src/store/globalStore.ts
@@ -11,7 +11,11 @@ export const useBoardStore = create<BoardStore>((set, get) => ({
   addBoard: (board) => {
     const { boards, past } = get();
     set({
-      boards: [...boards, board],
+      boards: [...boards, board].map((board) => ({
+        ...board,
+        todos: board.todos.sort((a, b) => a.order - b.order),
+      })),
+
       past: [...past, { type: "ADD_BOARD", payload: board }],
       future: [],
     });
@@ -20,8 +24,13 @@ export const useBoardStore = create<BoardStore>((set, get) => ({
     const { boards, past } = get();
     const deletedBoard = boards.filter((i) => i._id === boardId);
     const updatedBoards = boards.filter((i) => i._id !== boardId);
+
     set({
-      boards: updatedBoards,
+      boards: updatedBoards.map((board) => ({
+        ...board,
+        todos: board.todos.sort((a, b) => a.order - b.order),
+      })),
+
       past: [...past, { type: "DELETE_BOARD", payload: deletedBoard[0] }],
       future: [],
     });
@@ -34,7 +43,12 @@ export const useBoardStore = create<BoardStore>((set, get) => ({
     const updatedBoard = { ...boardToUpdate, title: newTitle };
 
     set({
-      boards: boards.map((board) => (board._id === id ? updatedBoard : board)), // boards 배열을 불변성 유지하며 갱신
+      boards: boards
+        .map((board) => (board._id === id ? updatedBoard : board))
+        .map((board) => ({
+          ...board,
+          todos: board.todos.sort((a, b) => a.order - b.order),
+        })), // boards 배열을 불변성 유지하며 갱신
       past: [
         ...past,
         {
@@ -47,7 +61,69 @@ export const useBoardStore = create<BoardStore>((set, get) => ({
       future: [],
     });
   },
+  addTodo: (newTodo) => {
+    const { boards, past } = get();
+    const boardToUpdate = boards.find((i) => i._id === newTodo.boardId);
+    if (!boardToUpdate) return;
 
+    const updatedBoard = {
+      ...boardToUpdate,
+      todos: [...boardToUpdate.todos, newTodo],
+    };
+    set({
+      boards: boards
+        .map((i) => (i._id === newTodo.boardId ? updatedBoard : i))
+        .map((board) => ({
+          ...board,
+          todos: board.todos.sort((a, b) => a.order - b.order),
+        })),
+      past: [...past, { type: "ADD_TODO", payload: newTodo }],
+      future: [],
+    });
+  },
+  updateTodo: (boardId, todoId, updatedTodo) => {
+    const { boards, past } = get();
+    const boardToUpdate = boards.find((i) => i._id === boardId);
+    if (!boardToUpdate) return;
+    const originalTodo = boardToUpdate.todos.filter((i) => i._id === todoId);
+    const updatedTodos = boardToUpdate.todos.map((todo) =>
+      todo._id === todoId ? { ...todo, ...updatedTodo } : todo
+    );
+    console.log(updatedTodos);
+
+    set({
+      boards: boards
+        .map((board) =>
+          board._id === boardId ? { ...board, todos: updatedTodos } : board
+        )
+        .map((board) => ({
+          ...board,
+          todos: board.todos.sort((a, b) => a.order - b.order),
+        })),
+      past: [...past, { type: "UPDATE_TODO", payload: originalTodo[0] }],
+      future: [],
+    });
+  },
+  deleteTodo: (boardId, todoId) => {
+    const { boards, past } = get();
+    const boardToUpdate = boards.find((i) => i._id === boardId);
+    if (!boardToUpdate) return;
+    const changeTodo = boardToUpdate.todos.filter((i) => i._id === todoId);
+    const updatedTodos = boardToUpdate.todos.filter((i) => i._id !== todoId);
+
+    set({
+      boards: boards
+        .map((board) =>
+          board._id === boardId ? { ...board, todos: updatedTodos } : board
+        )
+        .map((board) => ({
+          ...board,
+          todos: board.todos.sort((a, b) => a.order - b.order),
+        })),
+      past: [...past, { type: "DELETE_TODO", payload: { changeTodo } }],
+      future: [],
+    });
+  },
   undo: () => {
     const { past, future, boards } = get();
     if (past.length === 0) return;
@@ -56,7 +132,12 @@ export const useBoardStore = create<BoardStore>((set, get) => ({
 
     if (lastAction.type === "ADD_BOARD") {
       set({
-        boards: boards.filter((board) => board._id !== lastAction.payload._id),
+        boards: boards
+          .filter((board) => board._id !== lastAction.payload._id)
+          .map((board) => ({
+            ...board,
+            todos: board.todos.sort((a, b) => a.order - b.order),
+          })),
         past: past.slice(0, -1),
         future: [lastAction, ...future],
       });
@@ -75,7 +156,72 @@ export const useBoardStore = create<BoardStore>((set, get) => ({
       );
 
       set({
-        boards: updatedBoards,
+        boards: updatedBoards.map((board) => ({
+          ...board,
+          todos: board.todos.sort((a, b) => a.order - b.order),
+        })),
+        past: past.slice(0, -1),
+        future: [lastAction, ...future],
+      });
+    } else if (lastAction.type === "ADD_TODO") {
+      const { boardId, _id } = lastAction.payload;
+      const updatedBoards = boards.map((board) =>
+        board._id === boardId
+          ? { ...board, todos: board.todos.filter((todo) => todo._id !== _id) }
+          : board
+      );
+
+      set({
+        boards: updatedBoards.map((board) => ({
+          ...board,
+          todos: board.todos.sort((a, b) => a.order - b.order),
+        })),
+        past: past.slice(0, -1),
+        future: [lastAction, ...future],
+      });
+    } else if (lastAction.type === "UPDATE_TODO") {
+      const { boardId, _id } = lastAction.payload;
+      console.log(lastAction.payload);
+      const changeTodo = boards
+        .filter((i) => i._id === boardId)[0]
+        .todos.filter((j) => j._id === _id);
+      const updatedBoards = boards.map((board) =>
+        board._id === boardId
+          ? {
+              ...board,
+              todos: board.todos.map((todo) =>
+                todo._id === _id ? { ...lastAction.payload } : todo
+              ),
+            }
+          : board
+      );
+      console.log(updatedBoards);
+      console.log(changeTodo);
+      set({
+        boards: updatedBoards.map((board) => ({
+          ...board,
+          todos: board.todos.sort((a, b) => a.order - b.order),
+        })),
+        past: past.slice(0, -1),
+        future: [{ ...lastAction, payload: changeTodo[0] }, ...future],
+      });
+    } else if (lastAction.type === "DELETE_TODO") {
+      const { boardId } = lastAction.payload.changeTodo[0];
+      console.log(lastAction.payload.changeTodo);
+      console.log(boardId);
+      const updatedBoards = boards.map((board) =>
+        board._id === boardId
+          ? {
+              ...board,
+              todos: [...board.todos, lastAction.payload.changeTodo[0]],
+            }
+          : board
+      );
+      set({
+        boards: updatedBoards.map((board) => ({
+          ...board,
+          todos: board.todos.sort((a, b) => a.order - b.order),
+        })),
         past: past.slice(0, -1),
         future: [lastAction, ...future],
       });
@@ -93,7 +239,12 @@ export const useBoardStore = create<BoardStore>((set, get) => ({
       });
     } else if (nextAction.type === "DELETE_BOARD") {
       set({
-        boards: boards.filter((i) => i._id !== nextAction.payload._id),
+        boards: boards
+          .filter((i) => i._id !== nextAction.payload._id)
+          .map((board) => ({
+            ...board,
+            todos: board.todos.sort((a, b) => a.order - b.order),
+          })),
         past: [...past, nextAction],
         future: future.slice(1),
       });
@@ -104,6 +255,66 @@ export const useBoardStore = create<BoardStore>((set, get) => ({
       set({
         boards: [...changedTitle, nextAction.payload],
         past: [...past, { ...nextAction, payload: undoObject[0] }],
+        future: future.slice(1),
+      });
+    } else if (nextAction.type === "ADD_TODO") {
+      const { boardId } = nextAction.payload;
+      const updatedBoards = boards.map((board) =>
+        board._id === boardId
+          ? { ...board, todos: [...board.todos, nextAction.payload] }
+          : board
+      );
+
+      set({
+        boards: updatedBoards.map((board) => ({
+          ...board,
+          todos: board.todos.sort((a, b) => a.order - b.order),
+        })),
+        past: [...past, nextAction],
+        future: future.slice(1),
+      });
+    } else if (nextAction.type === "UPDATE_TODO") {
+      console.log(nextAction.payload);
+      const { boardId, _id } = nextAction.payload;
+      const originalTodo = boards
+        .filter((board) => board._id === boardId)[0]
+        .todos.filter((i) => i._id === _id);
+      console.log(originalTodo);
+      const updatedBoards = boards.map((board) =>
+        board._id === boardId
+          ? {
+              ...board,
+              todos: board.todos.map((todo) =>
+                todo._id === _id ? { ...nextAction.payload } : todo
+              ),
+            }
+          : board
+      );
+      console.log(updatedBoards);
+      set({
+        boards: updatedBoards.map((board) => ({
+          ...board,
+          todos: board.todos.sort((a, b) => a.order - b.order),
+        })),
+        past: [...past, { ...nextAction, payload: originalTodo[0] }],
+        future: future.slice(1),
+      });
+    } else if (nextAction.type === "DELETE_TODO") {
+      const { boardId, _id } = nextAction.payload.changeTodo[0];
+      console.log(nextAction.payload.changeTodo);
+      console.log(boardId);
+      console.log(_id);
+      const updatedBoards = boards.map((board) =>
+        board._id === boardId
+          ? { ...board, todos: board.todos.filter((i) => i._id !== _id) }
+          : board
+      );
+      set({
+        boards: updatedBoards.map((board) => ({
+          ...board,
+          todos: board.todos.sort((a, b) => a.order - b.order),
+        })),
+        past: [...past, nextAction],
         future: future.slice(1),
       });
     }
