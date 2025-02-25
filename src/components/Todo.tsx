1a506b324484997439e9e8aa5090fce1e2c35df5
evelo <evelo0702@gmail.com>
2025-02-25 21:09:35 +0900

    feat: Todo 중복 코드 분리

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState } from "react";
 import { Todo as TodoType } from "../types";
 import { useBoardStore } from "../store/globalStore";
@@ -7,17 +5,27 @@ import { useMotionValue, Reorder } from "framer-motion";
 
 interface TodoProps {
   data: TodoType;
+  boardEditMode: boolean;
 }
 
-export default function Todo({ data }: TodoProps) {
+export default function Todo({ data, boardEditMode }: TodoProps) {
   const y = useMotionValue(0);
   const [editMode, setEditMode] = useState(false);
+  const [selectedBoardId, setSelectedBoardId] = useState(data.boardId); // 선택된 보드 상태
   const [newTodo, setNewTodo] = useState({
     title: data.title,
     content: data.content,
     label: { id: data.label.id, title: data.label.title },
   });
-  const { updateTodo, deleteTodo, labelList } = useBoardStore();
+
+  const { boards, updateTodo, deleteTodo, labelList, moveTodoToBoard } =
+    useBoardStore();
+
+  const handleMoveTodo = () => {
+    if (selectedBoardId !== data.boardId) {
+      moveTodoToBoard(data._id, selectedBoardId);
+    }
+  };
 
   const handleUpdateTodo = () => {
     if (newTodo.title.trim() === "" || newTodo.content.trim() === "") {
@@ -30,9 +38,12 @@ export default function Todo({ data }: TodoProps) {
       content: newTodo.content,
       label: newTodo.label,
     };
-    updateTodo(data.boardId, data._id, changeTodo); // 수정된 내용 업데이트
+
+    updateTodo(data.boardId, data._id, changeTodo);
+    handleMoveTodo();
     setEditMode(false);
   };
+
   const handleDeleteTodo = () => {
     const res = confirm("할일을 삭제하시겠습니까?");
     if (res) {
@@ -40,105 +51,144 @@ export default function Todo({ data }: TodoProps) {
     }
   };
 
-  return (
-    <Reorder.Item value={data} id={data._id} style={{ y }}>
-      <div className=" p-3 rounded-lg shadow-sm  border-2 border-gray-400 my-2">
-        <div className="flex justify-between">
-          {editMode ? (
-            <input
-              type="text"
-              value={newTodo.title}
-              onChange={(e) =>
-                setNewTodo((prev) => ({ ...prev, title: e.target.value }))
-              }
-              className="text-lg font-semibold mt-1 p-1 border-b border-gray-400"
-            />
-          ) : (
-            <h2 className="text-lg font-semibold mt-1">{data.title}</h2>
-          )}
+  const renderTodoContent = () => (
+    <div className="flex justify-between">
+      {editMode ? (
+        <input
+          type="text"
+          value={newTodo.title}
+          onChange={(e) =>
+            setNewTodo((prev) => ({ ...prev, title: e.target.value }))
+          }
+          className="text-lg font-semibold mt-1 p-1 border-b border-gray-400"
+        />
+      ) : (
+        <h2 className="text-lg font-semibold mt-1">{data.title}</h2>
+      )}
 
-          <div>
-            {editMode ? (
-              <>
-                <button onClick={handleUpdateTodo} className=" ms-2">
-                  완료
-                </button>
-                <button onClick={() => setEditMode(false)} className="ms-2 ">
-                  취소
-                </button>
-              </>
-            ) : (
-              <>
-                <button onClick={() => setEditMode(true)} className="0 ms-2">
-                  수정
-                </button>
-                <button onClick={handleDeleteTodo} className="ms-2 ">
-                  삭제
-                </button>
-              </>
-            )}
-          </div>
-        </div>
-        <div className="text-xs text-gray-500 flex justify-between">
-          <span>{data.author}</span>
-          <span>{new Date(data.date).toLocaleDateString()}</span>
-        </div>
-        <div className="mt-2 flex flex-wrap gap-1">
+      {!boardEditMode && (
+        <div>
           {editMode ? (
-            <select
-              value={newTodo.label.id}
-              onChange={(e) =>
-                setNewTodo((prev) => ({
-                  ...prev,
-                  label: {
-                    id: e.target.value,
-                    title: e.target.selectedOptions[0].text,
-                  },
-                }))
-              }
-              className="text-xs px-2 py-1 border rounded-md"
-            >
-              <option value="">라벨 선택</option>
-              {labelList.map((label) => (
-                <option key={label._id} value={label._id}>
-                  {label.title}
-                </option>
-              ))}
-            </select>
+            <>
+              <button onClick={handleUpdateTodo} className="ms-2">
+                완료
+              </button>
+              <button onClick={() => setEditMode(false)} className="ms-2">
+                취소
+              </button>
+            </>
           ) : (
-            <span
-              className={`text-xs px-2 py-1 ${
-                (data.label.id === "67b9a48f67b4270871740d0b" &&
-                  "bg-red-200") ||
-                (data.label.id === "67b9a48f67b4270871740d0c" &&
-                  "bg-sky-200") ||
-                (data.label.id === "67b9a48f67b4270871740d0d" &&
-                  "bg-green-200") ||
-                (data.label.id == "67b9a48f67b4270871740d0e" &&
-                  "bg-yellow-200") ||
-                (data.label.id === "67b9a48f67b4270871740d0f" &&
-                  "bg-indigo-200")
-              } rounded-md`}
-            >
-              {data.label.title}
-            </span>
+            <>
+              <button onClick={() => setEditMode(true)} className="ms-2">
+                수정
+              </button>
+              <button onClick={handleDeleteTodo} className="ms-2">
+                삭제
+              </button>
+            </>
           )}
         </div>
-        {editMode ? (
-          <textarea
-            value={newTodo.content}
+      )}
+    </div>
+  );
+
+  const renderLabelAndBoardSelect = () => (
+    <div className="mt-2 flex flex-wrap gap-1">
+      {editMode ? (
+        <>
+          <select
+            value={newTodo.label.id}
             onChange={(e) =>
-              setNewTodo((prev) => ({ ...prev, content: e.target.value }))
+              setNewTodo((prev) => ({
+                ...prev,
+                label: {
+                  id: e.target.value,
+                  title: e.target.selectedOptions[0].text,
+                },
+              }))
             }
-            className="w-full p-2 mt-2 border border-gray-300 rounded-md"
-            rows={3}
-          />
-        ) : (
-          <p className="text-lg text-gray-600 mt-1 line-clamp-2">
-            {data.content}
-          </p>
-        )}
-      </div>
-    </Reorder.Item>
+            className="px-2 py-1 border rounded-md"
+          >
+            <option defaultChecked disabled>
+              라벨 변경
+            </option>
+            {labelList.map((label) => (
+              <option key={label._id} value={label._id}>
+                {label.title}
+              </option>
+            ))}
+          </select>
+          <select
+            value={selectedBoardId}
+            onChange={(e) => setSelectedBoardId(e.target.value)}
+            className="border p-1 rounded"
+          >
+            <option defaultChecked disabled>
+              보드 변경
+            </option>
+            {boards.map((board) => (
+              <option key={board._id} value={board._id}>
+                {board.title}
+              </option>
+            ))}
+          </select>
+        </>
+      ) : (
+        <span
+          className={`text-xs px-2 py-1 ${
+            (data.label.id === "67b9a48f67b4270871740d0b" && "bg-red-200") ||
+            (data.label.id === "67b9a48f67b4270871740d0c" && "bg-sky-200") ||
+            (data.label.id === "67b9a48f67b4270871740d0d" && "bg-green-200") ||
+            (data.label.id === "67b9a48f67b4270871740d0e" && "bg-yellow-200") ||
+            (data.label.id === "67b9a48f67b4270871740d0f" && "bg-indigo-200")
+          } rounded-md`}
+        >
+          {data.label.title}
+        </span>
+      )}
+    </div>
+  );
+
+  const renderTodoContentText = () => (
+    <p className="text-lg  mt-1 line-clamp-2">{data.content}</p>
+  );
+
+  const renderTodoTextArea = () => (
+    <textarea
+      value={newTodo.content}
+      onChange={(e) =>
+        setNewTodo((prev) => ({ ...prev, content: e.target.value }))
+      }
+      className="w-full p-2 mt-2 border border-gray-300 rounded-md"
+      rows={3}
+    />
+  );
+
+  return (
+    <>
+      {boardEditMode ? (
+        <Reorder.Item value={data} id={data._id} style={{ y }}>
+          <div className="p-3 rounded-lg shadow-sm bg-gray-300 my-2">
+            {renderTodoContent()}
+            <div className="text-sm  flex justify-between">
+              <span>{data.author}</span>
+              <span>{new Date(data.date).toLocaleDateString()}</span>
+            </div>
+            {renderLabelAndBoardSelect()}
+            {editMode ? renderTodoTextArea() : renderTodoContentText()}
+          </div>
+        </Reorder.Item>
+      ) : (
+        <div className="p-3 rounded-lg shadow-sm bg-gray-300 my-2">
+          {renderTodoContent()}
+          <div className="text-sm  flex justify-between">
+            <span>{data.author}</span>
+            <span>{new Date(data.date).toLocaleDateString()}</span>
+          </div>
+          {renderLabelAndBoardSelect()}
+          {editMode ? renderTodoTextArea() : renderTodoContentText()}
+        </div>
+      )}
+    </>
   );
 }
