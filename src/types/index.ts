13f65c1b2bcd27ef42345ee4eb2c8563df53a2fe
evelo <evelo0702@gmail.com>
2025-02-25 21:11:17 +0900

    feat: 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -32,8 +32,8 @@ interface Action {
 export interface BoardStore {
   boards: Board[];
   labelList: Label[];
-  past: Action[]; //undo
-  future: Action[]; // redo
+  past: Action[];
+  future: Action[];
   setBoards: (boards: Board[]) => void;
   setLabels: (labels: Label[]) => void;
   addBoard: (board: Board) => void;
@@ -44,6 +44,10 @@ export interface BoardStore {
   addTodo: (newTodo: Todo) => void;
   updateTodo: (boardId: string, todoId: string, updatedTodo: Todo) => void;
   deleteTodo: (boardId: string, todoId: string) => void;
+  ChangeOrderTodo: (boardId: string, todos: Todo[]) => void;
+  ChangeOrderBoards: (changeBoards: Board[]) => void;
+  syncBoardsWithServer: () => void;
+  moveTodoToBoard: (todoId: string, newBoardId: string) => void;
 }
 
 export interface ApiResponse<T = any> {
@@ -51,4 +55,10 @@ export interface ApiResponse<T = any> {
   message: string;
   status: number;
   data?: T;
+}
+
+export interface Task {
+  id: number;
+  label: string;
+  completed: boolean;
 }
